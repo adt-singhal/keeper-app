46c30389832fa2b00afa284ef7263c79b4d835f3
adt-singhal <142235775+adt-singhal@users.noreply.github.com>
2023-08-27 16:59:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import CopyrightOutlinedIcon from '@material-ui/icons/CopyrightOutlined';
 
 function Footer(){
     return (<footer>
-        <p><CopyrightOutlinedIcon/> {new Date().getFullYear()} Kartik Rathor</p>
+        <p><CopyrightOutlinedIcon/> {new Date().getFullYear()}</p>
     </footer>);
 }
 
